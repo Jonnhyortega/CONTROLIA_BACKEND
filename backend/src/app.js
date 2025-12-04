83aaf2be6df6cc152755e200d11d3718320126ac
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2025-12-04 14:26:24 -0300

    update app.js cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://controlia.com"],
+    origin: ["http://localhost:3000", "https://controlia.com", "https://controlia-software.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
